a260564695ea6f3790638a4f27bd708fd5a6b7b7
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-15 15:42:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,6 +35,7 @@ class Song
   end 
   
   def self.find_or_create_by_name(name)
-    return find_by_name(name) if find_by_name(name) != false 
+    return self.find_by_name(name) if find_by_name(name) != false 
+    self.create_by_name(name)
   end
 end
